929a7534dad4f74d1f9cb47cc535b7fa95d686c5
William Chen <williamchen@Williams-MacBook-Air.local>
2020-01-25 10:41:14 -0500

    added error handle and conrollers

---
+++ server/controller.js
@@ -0,0 +1,25 @@
+// need to add connection to db
+
+const cryptoController = {};
+
+
+// login controller
+cryptoController.login = (req, res, next) => {
+  // add login query here
+
+}
+
+// get market
+cryptoController.getMarket = (req, res, next) => {
+  // add get market query here
+
+}
+
+
+//get limit
+cryptoController.getLimit = (req, res, next) => {
+  // add get limit query here
+
+}
+
+module.exports = cryptoController;
--- server/server.js
+++ server/server.js
@@ -24,4 +24,18 @@ Code here
 
 */
 
+// error handler 
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
